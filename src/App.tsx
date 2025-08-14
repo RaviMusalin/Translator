d65c6c3c4fb4092495a6c3d5ab92c20a854d1b67
RaviMusalin <sheikhmusalin@gmail.com>
2025-08-14 02:44:19 -0400

    Hard coded languages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
+
+import {useState} from 'react'
 import './App.css'
+
+interface Language {
+  code: string;
+  name: string;
+}
+
+const Languages: Language[] = [
+  { code: "en", name: "English" },
+  { code: "es", name: "Spanish" },
+  { code: "fr", name: "French" },
+  { code: "de", name: "German" },
+  { code: "it", name: "Italian" },
+]
 
 function App() {
 
