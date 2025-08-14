c5796d9b7cfc0376c8ae7bdd29ad8d1ac50679b1
RaviMusalin <sheikhmusalin@gmail.com>
2025-08-14 03:06:32 -0400

    Structure of main app

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ interface Language {
   name: string;
 }
 
-const Languages: Language[] = [
+const LANGUAGES: Language[] = [
   { code: "en", name: "English" },
   { code: "es", name: "Spanish" },
   { code: "fr", name: "French" },
@@ -16,13 +16,49 @@ const Languages: Language[] = [
 ]
 
 function App() {
+  const [sourceLang, setSourceLang] = useState<string>("en"); // Language user types in
+  const [targetLang, setTargetLang] = useState<string>("es"); // output language
+  const [text, setText] = useState<string>(""); // user input
+  const [translation, setTranslation] = useState<string>(""); // translated user input (API translation result)
+  const [loading, setLoading] = useState<boolean>(true) // Boolean if we are waiting for translation
+  const [error, setError] = useState<string>("") // If API fails
 
    return (
     <>
       <h1>Instant Translator</h1>
-      <p>
-        React + Typescript practice
-      </p>
+      <p>React + Typescript practice</p>
+
+      <div style={{display: "flex", gap: "1rem", marginBottom: "1rem"}}>
+        <select value={sourceLang} onChange={(e) => setSourceLang(e.target.value)}>
+          {LANGUAGES.map((lang) => 
+            <option key={lang.code} value={lang.code}>
+              {lang.name}
+            </option>
+          )}
+        </select>
+
+        <select value={targetLang} onChange={(e) => setTargetLang(e.target.value)}>
+          {LANGUAGES.map((lang) => 
+            <option key={lang.code} value={lang.code}>
+              {lang.name}
+            </option>
+          )}
+        </select>
+      </div>
+
+      <textarea
+        rows={5}
+        style={{width: "100%"}}
+        placeholder='Type Something...'
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <div style={{ marginTop: "1rem", minHeight: "2rem"}}>
+        {loading ? <em>Translating...</em> : translation}
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </div>
+
     </>
   );
 }
